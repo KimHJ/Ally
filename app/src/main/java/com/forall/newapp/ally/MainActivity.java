a5d830bff4abe62f3839dfd6d0ce49081f8d7ffd
TestGit <muumuu.k@gmail.com>
2018-01-19 12:10:56 +0900

    Change Activity

---
+++ app/src/main/java/com/forall/newapp/ally/MainActivity.java
@@ -0,0 +1,16 @@
+package com.forall.newapp.ally;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by kim-hyonjun on 2018/01/19.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
